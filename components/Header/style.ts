abd777c01032d62a151ffb6ba5691fdd00dedc26
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-02-28 12:34:01 +0200

    Add files via upload

---
+++ components/Header/style.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    header: {
+      height:80,
+      paddingTop:38,
+      backgroundColor: 'coral'
+    },
+    title: {
+      textAlign: 'center',
+      color: '#fff',
+      fontSize:20,
+      fontWeight: 'bold',
+    },
+  
+  });
+  
+export default styles;
